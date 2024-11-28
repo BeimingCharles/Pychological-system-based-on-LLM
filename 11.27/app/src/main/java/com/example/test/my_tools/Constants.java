a44717922327638b158432d845143873ba49d5c0
Beiming <email@example.com>
2024-11-28 14:47:30 +0800

    c

---
+++ 11.27/app/src/main/java/com/example/test/my_tools/Constants.java
@@ -0,0 +1,10 @@
+package com.example.test.my_tools; // 根据你的实际包名进行修改
+
+public class Constants {
+    // 服务器的 IP 地址
+//    public static final String IP = "http://10.0.2.2:6671"; // 或者是实际的服务器 IP 地址
+    public static final String IP = "http://192.168.43.86:6671";    // 或者是实际的服务器 IP 地址
+
+    // 错误消息
+    public static final String NETERR = "网络错误，请稍后再试"; // 网络错误提示
+}
