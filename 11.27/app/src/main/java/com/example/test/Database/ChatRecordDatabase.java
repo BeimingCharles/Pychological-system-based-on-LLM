5dfe3acbdf9f4f1a93b465593ffcd9560921b8dd
Beiming <email@example.com>
2025-01-26 23:14:10 +0800

    qwe

--- 11.27/app/src/main/java/com/example/test/Database/ChatRecordDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.test.Database;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {ChatRecord.class}, version = 3, exportSchema = false)
-public abstract class ChatRecordDatabase extends RoomDatabase {
-    public abstract ChatRecordDao chatRecordDao(); // 注意方法名以小写开头
-}
